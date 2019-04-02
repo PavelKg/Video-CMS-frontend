79636f7b7614595f538a9a7bdaf7d64eb4f27ebd
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2019-04-02 18:15:47 +0600

    template for product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Video-CMS logo" src="../assets/video-icon.png" />
+    <HelloWorld msg="Welcome to Video-CMS" />
   </div>
 </template>
 
