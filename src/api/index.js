3df4ccce10f890a32d95bae92c0ee6e9a049f30a
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2020-02-22 12:26:11 +0600

    added auto logout if token exp1

--- src/api/index.js
+++ src/api/index.js
@@ -20,8 +20,6 @@ Api.interceptors.response.use(
   function(response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    console.log('response=', response.status)
-    //store.dispatch('LOGOUT').then(() => router.push(`/`))
 
     return response
   },
